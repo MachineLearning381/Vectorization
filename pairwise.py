c68e18ac089bd1a5f0af25b0c505c3625aa1270f
rlomax <richard.lomax@todaytix.com>
2020-09-29 06:59:10 -0400

    add numpy to distance function

---
+++ pairwise.py
@@ -0,0 +1,18 @@
+import numpy as np
+import datums as dat
+
+resloop = []
+xtrav = 0
+
+for x in dat.matrix:
+    res.append([])
+    inc = 0
+    while inc < len(dat.matrix)
+        ytrav = 0
+        val = 0
+        for y in x:
+            val += (dat.matrix[xtrav][ytrav] - dat.matrix[inc][ytrav]) ** 2
+        resloop[inc].append(val ** .5)
+        inc++
+
+resnump = np.array(dat.matrix)
